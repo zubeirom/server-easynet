527fd4a343448a6eadc194b7d41d2c9f6e8ac85c
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-27 21:01:17 +0200

    fix eslint

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-let bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const { Client } = require('pg')
+const client = new Client()
+
+const app = express();
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// cors
+app.use(cors());
