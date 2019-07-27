64c2ed221843c4d65b1c0d460608c00c56dbe567
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-27 04:15:03 +0200

    init commit

---
+++ db/easynetdb.sql
@@ -0,0 +1,10 @@
+CREATE TABLE person (
+    user_name VARCHAR(40) NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40),
+    biography VARCHAR(400),
+    age INT,
+    status VARCHAR(200),
+    PRIMARY KEY (user_name)
+);
+
