18dc31e5dec679a9f54eb001dc79f686f939acf1
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-27 23:16:47 +0200

    configure route index and install serializer

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('./db/index');
-
 const app = express();
 
 // Bodyparser
@@ -12,10 +10,6 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // cors
 app.use(cors());
-
-db.query('SELECT * FROM person', (err, res) => {
-  console.log(err, res);
-});
 
 
 const PORT = process.env.PORT || 3000;
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const db = require('./db/index');
+
+const router = express.Router();
+
+
+
+module.exports = router;
