948e4a9137e37a130c2856dfd22125882f9a3fa3
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-03 21:17:54 +0200

    by sending response we can update the client

--- routes/comments.js
+++ routes/comments.js
@@ -5,8 +5,8 @@ const express = require('express');
 // const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
 // const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
-// const JSONAPISerializer = require('jsonapi-serializer').Serializer;
-// const moment = require('moment');
+const JSONAPISerializer = require('jsonapi-serializer').Serializer;
+const moment = require('moment');
 // const fs = require('fs');
 
 const db = require('../db/index');
@@ -15,14 +15,25 @@ const db = require('../db/index');
 
 const router = express.Router();
 
+const CommentSerializer = new JSONAPISerializer('comment', {
+    attributes: ['author', 'message', 'created', 'post_id', 'timeAgo', 'comment_id'],
+    id: 'comment_id',
+    keyForAttribute: 'underscore_case',
+});
+
 router.post('/', asyncHandler(async (req, res, next) => {
     try {
         const {
             message, author, post_id,
         } = req.body;
-
-        await db.query(`INSERT INTO comment(message, author, post_id) VALUES('${message}', '${author}', ${post_id})`);
-        res.status(200).send([{ message: 'Succesfully saved comment' }]);
+        const postComment = await db.query(`INSERT INTO comment(message, author, post_id) VALUES('${message}', '${author}', ${post_id}) RETURNING *`);
+        const getAuthor = await db.query(`SELECT * FROM person WHERE user_name='${author}'`);
+        const comment = postComment.rows[0];
+        const authorObj = getAuthor.rows[0];
+        comment.author = authorObj;
+        comment.timeAgo = moment(comment.created).fromNow();
+        const commentJson = CommentSerializer.serialize(comment);
+        res.status(200).send(commentJson);
         next();
     } catch (error) {
         next(error);
